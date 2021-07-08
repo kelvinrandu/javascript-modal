74f8976e6355cdf522f3c16e3a3f6e47f4ebe800
kelvinrandu <randukelvin@gmail.com>
2021-07-08 13:05:51 +0300

    added toggle functionality

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-console.log('hrer')
+const modalBtn= document.querySelector('.btn');
+const modalOverlay = document.querySelector('.modal-overlay');
+const closeBtn = document.querySelector('.close-btn');
+
+modalBtn.addEventListener('click',function(){
+    modalOverlay.classList.add('open-modal');
+    console.log(modalOverlay);
+});
+
+closeBtn.addEventListener('click', function(){
+    modalOverlay.classList.remove('open-modal');
+
+})
